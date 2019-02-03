1b7fbf659e5a901a573862691f7aa3f03a30a2c2
Rohit Kaundal <rohit@vassagoconsultancy.com>
2019-02-03 16:28:29 +0530

    Algolia init

--- good-vibes/screens/SearchScreen.js
+++ good-vibes/screens/SearchScreen.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { InstantSearch } from 'react-instantsearch-native';
 
 class SearchScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>This is Search Screen</Text>
+    <InstantSearch
+          appId="latency"
+          apiKey="6be0576ff61c053d5f9a3225e2a90f76"
+          indexName="instant_search"
+        >
+          <Text>
+            Congrats 🎉! Your application is now connected to Algolia!
+          </Text>
+        </InstantSearch>
+
       </View>
     );
   }
